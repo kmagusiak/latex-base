aa1f8620dc84db2c7498c82373fe6c9edb9f8808
Krzysztof Magusiak <1943198+kmagusiak@users.noreply.github.com>
2011-06-12 06:52:51 +0200

    Generating vectorial images instead of png files. The used format is pdf, but eps support is added too.

--- script/latex-depgen.py
+++ script/latex-depgen.py
@@ -6,8 +6,10 @@ import os.path
 import sys
 
 GEN_FROM = 'Makefile.files'
-IMG_DIR = 'img'
-IMG_EXT_DEF = 'png'
+IMG_ROOT_DIR = 'img'
+IMG_EXT_NORMAL = ['eps', 'jpg', 'pdf', 'png']
+IMG_EXT_DEF = 'pdf' # must be in IMG_EXT_NORMAL
+LISTINGS_EXT = ['c', 'cpp', 'idl', 'java', 'h', 'py', 'tex']
 
 def getTexFileDep(filename, dep = None):
 	"""Generates dependencies for a tex file."""
@@ -15,11 +17,11 @@ def getTexFileDep(filename, dep = None):
 	if dep is None:
 		dep = set()
 	if os.path.splitext(filename)[1] != '.tex':
-		sys.stderr.write("getTexFileDep(%s): works only for tex files\n" \
+		sys.stderr.write("getTexFileDep(%s): works only for tex files\n"
 			% filename)
 		return dep
 	if not os.path.isfile(filename):
-		sys.stderr.write("getTexFileDep(%s): file does not exist\n" \
+		sys.stderr.write("getTexFileDep(%s): file does not exist\n"
 			% filename)
 		return dep
 	# analyse the lines of the given file
@@ -39,34 +41,36 @@ def getTexFileDep(filename, dep = None):
 			if line == '': continue
 			# -------------------------------
 			# document class
-			getTexFileDepMatch(dep, line, \
-				'\\\\documentclass' + match_opt_arg, ext = 'cls', optional = True)
+			getTexFileDepMatch(dep, line,
+				'\\\\documentclass' + match_opt_arg, ext = 'cls',
+				optional = True)
 			# style files
-			getTexFileDepMatch(dep, line, \
-				'\\\\usepackage'+match_opt_arg, ext = 'sty', optional = True)
+			getTexFileDepMatch(dep, line,
+				'\\\\usepackage'+match_opt_arg, ext = 'sty',
+				optional = True)
 			# include | input
-			getTexFileDepMatch(dep, line, \
+			getTexFileDepMatch(dep, line,
 				'\\\\(?:input|include)' + match_arg, ext = 'tex')
 			# bibliography
-			getTexFileDepMatch(dep, line, \
+			getTexFileDepMatch(dep, line,
 				'\\\\bibliography' + match_arg, ext = 'bib')
 			# includegraphics
-			getTexFileDepMatch(dep, line, \
-				'\\\\includegraphics' + match_opt_arg, \
-				ext = ['png', 'jpg', 'gif'], extdef = IMG_EXT_DEF)
+			getTexFileDepMatch(dep, line,
+				'\\\\includegraphics' + match_opt_arg,
+				ext = IMG_EXT_NORMAL, extdef = IMG_EXT_DEF)
 			# includeimage | includeimagefigure
-			getTexFileDepMatch(dep, line, \
-				'\\\\includeimage(?:base|figure)?' + match_opt_arg, \
-				ext = ['png', 'jpg', 'gif'], extdef = IMG_EXT_DEF, \
-				dir = IMG_DIR)
+			getTexFileDepMatch(dep, line,
+				'\\\\includeimage(?:base|figure)?' + match_opt_arg,
+				ext = IMG_EXT_NORMAL, extdef = IMG_EXT_DEF,
+				dir = IMG_ROOT_DIR)
 			# lstinputlisting
-			getTexFileDepMatch(dep, line, \
-				'\\\\lstinputlisting' + match_opt_arg, \
-				ext = ['c', 'cpp', 'idl', 'java', 'h', 'py', 'tex'])
+			getTexFileDepMatch(dep, line,
+				'\\\\lstinputlisting' + match_opt_arg,
+				ext = LISTINGS_EXT)
 			# -------------------------------
 	return dep
 
-def getTexFileDepMatch(dep, line, regex, \
+def getTexFileDepMatch(dep, line, regex,
 		ext = '', extdef = '', dir = '',
 		invalid = '.*#\\d+.*|.*\\\\.*', optional = False):
 	"""Tries to match a dependency on a tex file line."""
@@ -74,11 +78,13 @@ def getTexFileDepMatch(dep, line, regex, \
 	m = re.search(regex, line)
 	if m is None: return
 	f = m.group(1)
-	if f == '' or (invalid is not None and re.match(invalid, f) is not None): return
+	if (f == '' or (
+		invalid is not None and
+		re.match(invalid, f) is not None)): return
 	# add the directory
 	if dir != '': f = os.path.join(dir, f)
 	# check the extension
-	if type(ext) != list:
+	if not isinstance(ext, list):
 		ext = [ext]
 		if extdef == '': extdef = ext[0]
 	# find the files
@@ -89,6 +95,7 @@ def getTexFileDepMatch(dep, line, regex, \
 	if len(found) > 0:
 		f = found[0]
 	else:
+		# try to set the default extension
 		if extdef != '':
 			f += '.' + extdef
 		if optional and not os.path.isfile(f):
@@ -143,7 +150,8 @@ if __name__ == '__main__':
 		writeDependencies(out, file, d)
 		dd |= set(d)
 		dd.add(file)
-	accept = lambda f: os.path.splitext(f)[1] in ['.' + ext for ext in ['bib', 'cls', 'tex']]
+	accept = lambda f: (os.path.splitext(f)[1] in
+		['.' + ext for ext in ['bib', 'cls', 'tex']])
 	dd = set(filter(accept, dd))
 	out.write("# Dependencies of this file\nMakefile.d:")
 	out.write(" " + GEN_FROM)
